04ca24c14d4249bfd30f8536ad550d58247835de
Vardandatasciences <vdatasciences@gmail.com>
2025-05-25 14:19:32 +0530

    Update Cypress config paths for frontend folder

--- cypress.config.js
+++ cypress.config.js
@@ -3,8 +3,10 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: "http://localhost:8081",
+    specPattern: "frontend/cypress/integration/**/*.js",
+    supportFile: "frontend/cypress/support/e2e.js",
     setupNodeEvents(on, config) {
-      // implement node event listeners here if needed
+      // your node event handlers here
     },
   },
 });
